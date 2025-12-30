6f791b42262b6c32ba9ef0845a358f06d0502cc3
nisa <nisa@4visionmedia.com>
2025-12-30 09:39:29 +0700

    update base deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   base: "https://github.com/nisaagustiina/e-plantShopping.git",
   plugins: [react()],
 })
